0daee83cc5d92f584d2b7b0ad0acf9fda14dc6ae
LJC <2433198567@qq.com>
2023-06-20 16:52:33 +0800

    tags栏实现

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -8,7 +8,7 @@
       ></el-button>
       <el-breadcrumb separator="/">
         <el-breadcrumb-item v-for="item in tags" :key="item.path" 
-        :to="{ path: item.path }">{{item.label}}
+        >{{item.label}}
         </el-breadcrumb-item>
 
       </el-breadcrumb>
--- src/components/CommonTag.vue
+++ src/components/CommonTag.vue
@@ -6,8 +6,11 @@
       :closable="item.name !== 'home'"
       :type="item.type"
       :effect="$route.name===item.name ? 'dark' :'plain'"
+      @close="handleClose(item)"
+      @click="changeMenu(item)"
+      size="small"
     >
-      >
+      
       {{ item.label }}
     </el-tag>
   </div>
@@ -22,6 +25,36 @@ export default {
 
     }
   },
+  methods:{
+    handleClose(tag){
+      const t = this.tags
+      if(this.$route.name === 'home'){
+        //splice参数第二个为一代表只删除一个元素
+        t.splice(t.indexOf(tag),1)
+      }
+      else if (this.$route.name !== tag.name ){
+        t.splice(t.indexOf(tag),1)
+      }
+      else if(this.$route.name === t[t.length-1].name){
+        t.splice(t.indexOf(tag),1)
+        this.$router.push(t[t.length-1])
+      }
+      
+      else{
+        const i = t.indexOf(tag)
+        t.splice(t.indexOf(tag),1)
+         
+        this.$router.push(t[i-1])
+      }
+    },
+    changeMenu(item){
+
+      if (this.$route.path !== item.path && !(this.$route.path === '/home' && (item.path === '/'))) {
+        this.$router.push(item.path)
+      }
+      this.$store.commit('selectMenu',item)
+    }
+  },
   computed:{
     ...mapState({
       tags: state=> state.tab.tabList
@@ -31,4 +64,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .tabs{
+    padding: 20px;
+    .el-tag{
+      margin-right: 15px;
+      cursor: pointer;
+    }
+  }
 </style>
