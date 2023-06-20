351208b0d7a4098e20a5d27b65d52849b833c7ff
LJC <2433198567@qq.com>
2023-06-20 11:08:32 +0800

    vuex实现数据流转的面包屑基本实现

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="header-container">
     <div class="l-content">
-      <el-button @click="handleMenu" icon="el-icon-menu"  size="mini"></el-button>
-      <span class="text">首页</span>
+      <el-button
+        @click="handleMenu"
+        icon="el-icon-menu"
+        size="mini"
+      ></el-button>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item v-for="item in tags" :key="item.path" 
+        :to="{ path: item.path }">{{item.label}}
+        </el-breadcrumb-item>
+
+      </el-breadcrumb>
     </div>
     <div class="r-content">
       <el-dropdown>
         <span class="el-dropdown-link">
-          <img class="user" src="../assets/image/QQ图片20230605191623.jpg" alt="">
+          <img
+            class="user"
+            src="../assets/image/QQ图片20230605191623.jpg"
+            alt=""
+          />
         </span>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>个人中心</el-dropdown-item>
           <el-dropdown-item>退出登录</el-dropdown-item>
-        
         </el-dropdown-menu>
       </el-dropdown>
     </div>
@@ -20,15 +32,24 @@
 </template>
 
 <script>
+//辅助函数`mapState`
+import {mapState} from 'vuex'
 export default {
   data() {
     return {};
   },
-  methods:{
-    handleMenu(){
-      this.$store.commit('collapseMenu')
+  methods: {
+    handleMenu() {
+      this.$store.commit("collapseMenu");
       console.log(this.$store.state.tab);
-    }
+    },
+  },
+  computed:{
+    //tags:别名 后面跟函数 再用拓展运算符...进行解构
+    // 这样就拥有了state中的tabList 别名tags
+    ...mapState({
+      tags: state => state.tab.tabList
+    })
   }
 };
 </script>
@@ -46,10 +67,28 @@ export default {
     margin: 0 20px;
     font-size: 14px;
   }
-  .r-content .user{
+  .r-content .user {
     height: 40px;
     width: 40px;
     border-radius: 50%;
+  }
+  .l-content{
+    button{
+      margin-right: 20px;
+    }
+    display: flex; 
+    align-items: center;
+    /deep/ .el-breadcrumb__item{
+      .el-breadcrumb__inner{
+        font-weight: normal ;
+        color:#666;
+      }
+      &:last-child{
+        .el-breadcrumb__inner{
+          color: #fff;
+        }
+      }
+    }
   }
 }
 </style>
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,11 +1,33 @@
 export default{
   state :{
-    isCollapse:false //控制菜单展开还是收起
+    tabList:[
+      {
+        path: "/",
+        name: "home",
+        label: "首页",
+        icon: "s-home",
+        url: "Home/Home",
+      },
+    ],//面包屑数据
+    isCollapse:false, //控制菜单展开还是收起
   },
 
   mutations:{
     collapseMenu(state) {
       state.isCollapse = !state.isCollapse
+    },
+    //更新面包屑数据
+    selectMenu(state,val){
+      console.log();
+
+      //判断添加的数据是否为首页
+      if(val.name!=='home'){
+        const index = state.tabList.findIndex(item=> item.name === val.name)
+
+        if(index === -1){
+          state.tabList.push(val)
+        }
+      }
     }
   }
 }
